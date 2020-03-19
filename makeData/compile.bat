59ced4dcebaebf43e06d758f73f4a8aea0bb23f8
ignatandrei <ignatandrei@yahoo.com>
2020-03-19 22:37:49 +0200

    copy js files

--- makeData/compile.bat
+++ makeData/compile.bat
@@ -1,5 +1,6 @@
 docker build -t ignatandrei/wfh .. -f ./compile.txt  
 docker run -d --name wfh  ignatandrei/wfh
 docker cp wfh:/usr/src/obj/all.md .
+docker cp wfh:/usr/src/obj/all.js .
 docker container kill wfh
 docker container rm wfh
