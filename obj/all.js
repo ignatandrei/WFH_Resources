f9b20ccef20a518b175c8c93f42ef8406391ca95
GitHub Action <action@github.com>
2020-03-19 21:00:56 +0000

    generate html

--- obj/all.js
+++ obj/all.js
@@ -1,46 +1,45 @@
 [
 	{
-		id: 1,
 		tags: "",
 		category: "Country",
 		subCategory: "China",
 		name: "Zoom",
 		links: [
 			"https://blog.zoom.us/wordpress/2020/02/26/zoom-commitment-user-support-business-continuity-during-coronavirus-outbreak/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.213Z"
 	},
 	{
-		id: 2,
 		tags: "",
 		category: "Country",
 		subCategory: "France",
 		name: "Pornhub",
 		links: [
 			"https://twitter.com/Pornhub/status/1239724008322539522"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 3,
 		tags: "",
 		category: "Country",
 		subCategory: "Italy",
 		name: "Pornhub",
 		links: [
 			"https://twitter.com/Pornhub/status/1238130264477491200"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 4,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Copii",
 		links: [
 			"https://tikaboo.ro/jocuri-pentru-copii-in-casa/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 5,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
@@ -49,20 +48,20 @@
 			"https://centruldeparenting.ro/vacanta-virtuala-stam-acasa-muzee-ce-pot-fi-vizitate-online/",
 			"https://www.mnar.arts.ro/descopera/galerii-permanente",
 			"http://www.cimec.ro/muzee/muzee-cu-tur-virtual.html"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 6,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Diagnosticare online",
 		links: [
 			"https://spitalul-colentina.medicentrum.ro/solicita-evaluare/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 7,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
@@ -70,60 +69,60 @@
 		links: [
 			"https://laurentiumihai.ro/carti-audio-online-gratuite/",
 			"http://1cartepesaptamana.ro/carti-audio-audiobook-uri-gratuite/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 8,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Livrare mincare",
 		links: [
 			"https://backtoromania.com/2020/03/11/hipermarketuri-care-livreaza-acasa/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 9,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Geografie",
 		links: [
 			"https://geogra.ro/joc/ro/index.php"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 10,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Idei creative",
 		links: [
 			"http://www.creatissimo.ro/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 11,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Inițiative civice/private gratuite din România care ajută pe durata Coronavirus",
 		links: [
 			"https://docs.google.com/document/d/1pX4V6hTx9f4Oe8aNofhx_ePeDAE8PNEYRjox4NhnW6w/edit?fbclid=IwAR2ECHo23Hgh0WEGY-Jhsy05yM2oUZ10FrQSbOThslveaEoiru-1qBkDuPo"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 12,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Scoala pe net",
 		links: [
 			"https://scoalapenet.ro/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 13,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
@@ -132,10 +131,10 @@
 			"https://andreicismaru.ro/o-mana-de-ajutor/",
 			"https://www.facebook.com/AsociatiaRomanaDePsihoterapieIntegrativa/photos/a.1480599812161667/2610736969147940/?type=3&theater",
 			"http://xn--mlin-0sa.ro/asistenta-covid-19"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 14,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
@@ -144,220 +143,220 @@
 			"https://www.facebook.com/depreHUB/photos/a.138205610881482/211587666876609/?type=3&theater",
 			"https://www.facebook.com/Fundatia.Sensiblu/photos/a.469191036467190/2996699150383020/?type=3&theater",
 			"https://www.facebook.com/myatlasapp/photos/a.1577589982500915/2510672452525992/?type=3&theater"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 15,
 		tags: "",
 		category: "Country",
 		subCategory: "Romania",
 		name: "Conferinte inregistrate",
 		links: [
 			"https://techweek.ro/2019/colectia-de-sesiuni-business-summits-2018-2019/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.229Z"
 	},
 	{
-		id: 16,
 		tags: "",
 		category: "Country",
 		subCategory: "Spain",
 		name: "Pornhub",
 		links: [
 			"https://twitter.com/Pornhub/status/1239724235133718528"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.230Z"
 	},
 	{
-		id: 17,
 		tags: "",
 		category: "Entertainment",
 		subCategory: "Movies",
 		name: "Open Culture",
 		links: [
 			"http://www.openculture.com/freemoviesonline"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.230Z"
 	},
 	{
-		id: 18,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Adobe",
 		name: "Adobe Connect conferencing",
 		links: [
 			"https://theblog.adobe.com/adobe-enables-distance-learning-globally-schools-impacted-covid-19/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.232Z"
 	},
 	{
-		id: 19,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Avid",
 		name: "Media Composer and others",
 		links: [
 			"https://www.avid.com/covid-19"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.232Z"
 	},
 	{
-		id: 20,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Cisco",
 		name: "Cisco WebEx free",
 		links: [
 			"https://help.webex.com/en-us/n80v1rcb/Cisco-Webex-Available-Free-in-These-Countries-COVID-19-Response"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.232Z"
 	},
 	{
-		id: 21,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Cloudflare",
 		name: "Cloudflare",
 		links: [
 			"https://blog.cloudflare.com/cloudflare-for-teams-free-for-small-businesses-during-coronavirus-emergency/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.233Z"
 	},
 	{
-		id: 22,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Discord",
 		name: "Go Live",
 		links: [
 			"https://blog.discordapp.com/helping-out-where-we-can-3a5fb7bac77a?gi=c308eedc43a1"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.233Z"
 	},
 	{
-		id: 23,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Google",
 		name: "G Suite",
 		links: [
 			"https://cloud.google.com/blog/products/g-suite/helping-businesses-and-schools-stay-connected-in-response-to-coronavirus"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.233Z"
 	},
 	{
-		id: 24,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Logmein",
 		name: "GotoMeeting",
 		links: [
 			"https://blog.gotomeeting.com/coronavirus-disruptions-and-support/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.233Z"
 	},
 	{
-		id: 25,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Loom",
 		name: "Loom",
 		links: [
 			"https://www.loom.com/blog/coronavirus-response"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.234Z"
 	},
 	{
-		id: 26,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Microsoft",
 		name: "Teams",
 		links: [
 			"https://www.microsoft.com/en-us/microsoft-365/blog/2020/03/05/our-commitment-to-customers-during-covid-19/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.234Z"
 	},
 	{
-		id: 27,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Swazm",
 		name: "Swazm",
 		links: [
 			"https://swazm.com/ro/launchpad"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.234Z"
 	},
 	{
-		id: 28,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "ToonBoom",
 		name: "Harmony & Storyboard Pro",
 		links: [
 			"https://www.toonboom.com/covid19"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.234Z"
 	},
 	{
-		id: 29,
 		tags: "",
 		category: "FreeSoftware",
 		subCategory: "Zoho",
 		name: "Zoho remotely",
 		links: [
 			"https://www.zoho.com/remotely/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.235Z"
 	},
 	{
-		id: 30,
 		tags: "",
 		category: "Kids",
 		subCategory: "English",
 		name: "NASA",
 		links: [
 			"https://www.nasa.gov/kidsclub/index.html"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.235Z"
 	},
 	{
-		id: 31,
 		tags: "",
 		category: "Learn",
 		subCategory: "Conferences",
 		name: "ECommerce",
 		links: [
 			"https://ecommercegrowthsummit.com/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 32,
 		tags: "",
 		category: "Learn",
 		subCategory: "Conferences",
 		name: "General",
 		links: [
 			"https://www.exoworld.live/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 33,
 		tags: "",
 		category: "Learn",
 		subCategory: "Courses",
 		name: "www.freecodecamp.org",
 		links: [
 			"https://www.freecodecamp.org/news/here-are-380-ivy-league-courses-you-can-take-online-right-now-for-free-9b3ffcbd7b8c/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 34,
 		tags: "",
 		category: "Learn",
 		subCategory: "Courses",
 		name: "Udemy",
 		links: [
 			"https://docs.google.com/spreadsheets/d/1OqdaJVNcqZSK0RaNvoty8S2HtqThTY8vTEybJsBmmIg/edit#gid=485968471"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 35,
 		tags: "",
 		category: "Learn",
 		subCategory: "Courses",
 		name: "365 Data Science",
 		links: [
 			"https://365datascience.com/courses/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 36,
 		tags: "",
 		category: "Learn",
 		subCategory: "Courses",
@@ -365,66 +364,67 @@
 		links: [
 			"https://www.coursera.org/courses?query=free",
 			"https://www.coursera.org/collections/popular-free-courses"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.236Z"
 	},
 	{
-		id: 37,
 		tags: "",
 		category: "Learn",
 		subCategory: "DigitalLibrary",
 		name: "World digital library",
 		links: [
 			"https://www.wdl.org/en/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	},
 	{
-		id: 38,
 		tags: "",
 		category: "Learn",
 		subCategory: "DigitalLibrary",
 		name: "ITShare",
 		links: [
 			"https://itshare.ro/download"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	},
 	{
-		id: 39,
 		tags: "",
 		category: "Learn",
 		subCategory: "DigitalLibrary",
 		name: "Arts and culture",
 		links: [
 			"https://artsandculture.google.com/"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	},
 	{
-		id: 40,
 		tags: "",
 		category: "Learn",
 		subCategory: "DigitalLibrary",
 		name: "Cambridge",
 		links: [
 			"https://www.cambridge.org/core/what-we-publish/textbooks"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	},
 	{
-		id: 41,
 		tags: "",
 		category: "Learn",
 		subCategory: "Museums",
 		name: "Multiple",
 		links: [
 			"https://www.travelandleisure.com/attractions/museums-galleries/museums-with-virtual-tours"
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	},
 	{
-		id: 42,
 		tags: "",
 		category: "MapsAndData",
 		subCategory: "Romania",
 		name: "Harta",
 		links: [
 			""
-		]
+		],
+		dateCreated: "2020-03-19T21:00:52.237Z"
 	}
 ]
