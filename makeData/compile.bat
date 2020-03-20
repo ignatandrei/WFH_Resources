b66c0a4a7d093dcf708be75ce1c8523f662d1c11
ignatandrei <ignatandrei@yahoo.com>
2020-03-20 16:25:00 +0200

    extract data

--- makeData/compile.bat
+++ makeData/compile.bat
@@ -2,6 +2,7 @@ docker build -t ignatandrei/wfh .. -f ./compile.txt
 docker run -d --name wfh  ignatandrei/wfh
 docker cp wfh:/usr/src/obj/all.md .
 docker cp wfh:/usr/src/obj/all.js .
+docker cp wfh:/usr/src/WFHResourcesApp/src/app/table-wfh/data.ts .
 docker cp wfh:/usr/src/obj/WFHAng/ ./WFHAng/
 docker container kill wfh
 docker container rm wfh
