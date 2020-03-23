a7a6ff5664ef2a4e0cf0eab6fd83f15d1db06b50
ignatandrei <ignatandrei@yahoo.com>
2020-03-23 12:33:21 +0200

    adding about

--- WFHResourcesApp/src/app/app.module.ts
+++ WFHResourcesApp/src/app/app.module.ts
@@ -13,9 +13,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, TableWFHComponent, WfhNavigationComponent],
+  declarations: [AppComponent, TableWFHComponent, WfhNavigationComponent, AboutComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
