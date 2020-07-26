ae5acefd6406cc159891460c49bd2b30beec4a78
ignatandrei <ignatandrei@yahoo.com>
2020-07-26 11:35:19 +0300

    percentage

--- WFHResourcesApp/src/app/covid-table-data/covid-table-data.component.ts
+++ WFHResourcesApp/src/app/covid-table-data/covid-table-data.component.ts
@@ -5,7 +5,7 @@ import { tap, switchMap, concatMap, mergeMap, map } from 'rxjs/operators';
 import { CovidData } from 'src/codvid';
 import { country } from 'src/countryList';
 import { forkJoin } from 'rxjs';
-
+import * as CountryImport from '../../countryList';
 @Component({
   selector: 'app-covid-table-data',
   templateUrl: './covid-table-data.component.html',
@@ -25,11 +25,7 @@ export class CovidTableDataComponent implements OnInit {
   constructor(    private covidDataService: CovidDataService) { }
   changeStatus(s: string) {
     this.statusSelected = s;
-    window.alert(this.statusSelected);
     this.reloadData();
-    window.alert(this.statusSelected);
-    // window.alert(this.statusSelected);
-    //this.getCovidDataAll(this.countrySelected.map((it) => it?.Slug));
   }
   reloadData(){
     this.lastItems.length=0;
@@ -45,9 +41,13 @@ export class CovidTableDataComponent implements OnInit {
           
           this.displayData.set(item[0] , item[1]);
           var last=item[1][0];
+          const c = CountryImport.Countries.find((it) => it.name === last.Country);
+          last['CasesPerPopulation'] = last.Cases * 1000000 / c.population2020;
+          last['population'] = c.population2020;
           this.lastItems.push(last);
+          
         }
-        this.lastItems  = this.lastItems.sort((a,b)=>  b.Cases - a.Cases);
+        this.lastItems  = this.lastItems.sort((a,b)=>  b['CasesPerPopulation'] - a['CasesPerPopulation']);
         // window.alert(this.displayData.size);
       })
     )
